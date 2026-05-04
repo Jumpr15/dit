fc51a24b9618288efff2214c0ea3771c33192396
Jumpr15 <chordataosteichthyes@gmail.com>
2026-05-04 12:31:41 +1000

    changed upload

--- src/upload.py
+++ src/upload.py
@@ -1,4 +1,7 @@
 from nets.DiT import DIT
 
-model = DIT.load_from_checkpoint("./model_ckpts/epoch=0-step=700.ckpt")
-model.push_to_hub("Jumpr/artbench-dit-700-steps")
+# set model checkpoint
+model = DIT.load_from_checkpoint("./model_ckpts/")
+
+#
+model.push_to_hub("Jumpr/anime-dit-checkpoints")
