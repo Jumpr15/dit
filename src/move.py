43e1077854d04b4b9d6ca5e8a676353f0141cea2
Jumpr15 <chordataosteichthyes@gmail.com>
2026-05-07 10:16:22 +1000

    removed /

--- src/move.py
+++ src/move.py
@@ -3,11 +3,12 @@ import shutil
 
 #Path of source directory & destination directory
 src_directory = '/danbooru_faces/danbooru_eva_faces'
-dst_directory = '/images'
+dst_directory = 'images'
 
 # Extract file from Source directory and copy to Destination directory
 for file_num in range(4):   
-     for file in os.listdir(f'danbooru_faces/danbooru_eva_faces/{file_num}'):
+     src_directory = f'danbooru_faces/danbooru_eva_faces/{file_num}'
+     for file in os.listdir(src_directory):
           src_file = os.path.join(src_directory, file)
           dest_file = os.path.join(dst_directory, file)
 
